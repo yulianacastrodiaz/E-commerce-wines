01c8c1de377ba75be274b8c3cea563d373d81d8e
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-09-16 22:32:58 -0300

    added navbar and modal

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Cards from "../components/Cards";
-
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Cards />
     </main>
   );
