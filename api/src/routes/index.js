0d070edfcb00ef5ed0dd13cee81e3d7c2bf6970d
jcantini <jorge.cantinigmail.com>
2021-09-17 19:13:29 -0300

    Indexjs con el agregado de la ruta a producto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const categoriesRouter = require('./categories')
+const productRouter = require('./product-Crud')
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/categories', categoriesRouter);
+
+router.use('/product',productRouter);
 
 module.exports = router;
