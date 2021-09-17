40a62802b497031e9e94cac5b2a9f7399dc1e077
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-16 22:31:02 -0500

    Route GET/categories

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,20 +1,35 @@
 const { Router } = require('express');
-const { Categorie } = require('../db')
-
+const { Category } = require('../db')
+const { SubCategory } = require('../db')
 const router = Router();
 
-router.get('/',(req, res) => {
-  res.json("soy la ruta get")
+router.get('/', async(req, res) => {
+  const { name } = req.query
+  try {
+    if (name) {
+      const categorie = await Category.findOne({ where: { name }, include: SubCategory })
+      if (categorie) {
+        res.json(categorie)
+      } else {
+        res.status(404).json({msg: `No se encontró la categoria ${name}`})
+      }
+    } else {
+      const allCategories = await Category.findAll({include: SubCategory});
+      res.json(allCategories)
+    }
+  } catch (error) {
+    res.status(404).json(error)
+  }
 });
 
-router.post('/', async(req, res) => {
-  const {name} = req.body;
+router.post('/', async (req, res) => {
+  const { name } = req.body;
   try {
-    if(name){
-      const newCategorie = await Categorie.create({
+    if (name) {
+      const newCategory = await Category.create({
         name
       })
-      res.json(newCategorie)
+      res.json(newCategory)
     }
   } catch (error) {
     console.log(error);
