bfe5053a1e25459ffbef5664f42c3b202ed2dc11
unknown <fragusan2@gmail.com>
2021-09-17 10:55:47 -0300

    intento de filtrados

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const categoriesRouter = require('./categories')
+const filtersRouter = require('./filters')
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/categories', categoriesRouter);
+router.use('/filter', filtersRouter);
 
 module.exports = router;
