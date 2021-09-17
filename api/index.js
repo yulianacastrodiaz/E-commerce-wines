b8915d696ff744521a9c3124dfe488d53762146d
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-17 08:34:06 -0500

    preset and preload of product

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const preloadCategories = require('./src/preload/preloadCategories')
 const preloadGrapes = require('./src/preload/preloadGrapes')
 const preloadSubCategories = require('./src/preload/preloadSubCategories')
 const presetCategoryToSubCategory = require('./src/preset/presetCategoryToSubCategory')
+const preloadProducts = require('./src/preload/preloadProducts')
+const presetProducts = require('./src/preset/presetProducts')
 
 // Syncing all the models at once.
 const force = true;
@@ -18,16 +20,16 @@ try {
         console.log('precarga de subcategorías: done :)');
         const grapes = await preloadGrapes();  
         console.log('precarga de uvas: done :)');
-          // presetCategoryToSubCategory()
+        const products = await preloadProducts();
+        console.log('precarga de productos: done :)');
         const setCategoriesToSubCategories = await presetCategoryToSubCategory();
+        const setProducts = await presetProducts();
+        console.log('preseteo de productos: done :)');
       }
       server.listen(3001, () => {
         console.log('%s listening at 3001'); // eslint-disable-line no-console
       });
     })
-    // .then(() => {
-    //   presetCategoryToSubCategory()
-    // })
 } catch (error) {
   console.log(error)
 }
