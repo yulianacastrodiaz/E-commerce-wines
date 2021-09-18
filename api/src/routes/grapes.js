1e9216b986cd89888ba29f8c55cd0929d4d733d7
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-18 06:57:33 -0500

    Route POST/grapes

--- api/src/routes/grapes.js
+++ api/src/routes/grapes.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const { Router } = require('express');
 const { Grape } = require('../db')
 const { Product } = require('../db')
@@ -13,5 +14,20 @@ try {
   res.status(404).json
 }
 
+
+try {
+  router.post('/', async(req, res) => {
+    const { name } = req.body;
+    if(name) {
+      const newGrape = await Grape.create({ name })
+      res.json(newGrape)
+    } else {
+      res.status(400).json({msg: "Por favor manda el nombre de la uva"})
+    }
+  })
+} catch (error) {
+  console.log(error)
+  res.status(404).json(error)
+}
 
 module.exports = router;
