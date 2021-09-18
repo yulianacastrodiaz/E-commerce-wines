3f5ccecc74ff67e3754eaf33cf68730e39ab26b9
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-17 19:30:35 -0500

    refactor files

--- api/src/routes/subCategories.js
+++ api/src/routes/subCategories.js
@@ -5,9 +5,6 @@ const { Category } = require('../db')
 
 const router = Router();
 
-//Con esta pequeña funcion, seteo las primeras letras de los string a mayusculas
-// const firstUpperCase = function (mayus) { return mayus.replace(/\b\w/g, l => l.toUpperCase()) }
-
 try {
   router.get('/', async (req, res) => {
     let { type } = req.query
@@ -41,24 +38,5 @@ try {
   console.log(error)
   res.status(404).json(error)
 }
-// router.get('/grape', async (req, res) => {
-// console.log("esto es lo que hay en db grape:", grape)
-//     let { name } = req.query
-//     if (name) {
-//         name = firstUpperCase(name)
-//         const uva = await grape.findAll({
-//             where: { name }
-//         })
-//         return (uva) ? res.json(uva)
-//             : res.status(404).json(`No tenemos existencias en nuestro catalogo del tipo de uva que estas buscando`)
-//     }
-//     const alluvas = await grape.findAll()
-//     res.json(alluvas)
-// })
-
-// router.get('/products', async (req, res) => {
-// res.json(`esperese mijito que esta ruta aun no funciona`)
-// })
-
 
 module.exports = router;
