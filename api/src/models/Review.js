8d2aa38603d84e80982bbbda906750621516feda
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-20 10:04:21 -0500

    Initial model review

---
+++ api/src/models/Review.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo correspondiente a la tabla videogame
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('review', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  });
+};
