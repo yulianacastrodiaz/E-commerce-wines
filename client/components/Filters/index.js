dc1558605fe175296f8024a7d4772824f6d8e04d
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-09-21 17:01:51 -0300

    Login and fixed classNames

--- client/components/Filters/index.js
+++ client/components/Filters/index.js
@@ -9,12 +9,12 @@ const Filters = () => {
 
   return (
     <div className="flex justify-center -mt-11 bg-white">
-      <div class="navbar bg-white text-black-content rounded-box">
-        <div class="hidden px-2 mx-2 navbar-center lg:flex">
-          <div class="flex items-stretch">
+      <div className="navbar bg-white text-black-content rounded-box">
+        <div className="hidden px-2 mx-2 navbar-center lg:flex">
+          <div className="flex items-stretch">
             {categories &&
               categories.map((e) => (
-                <a class="btn btn-ghost btn-sm rounded-btn">{e.name}</a>
+                <a className="btn btn-ghost btn-sm rounded-btn">{e.name}</a>
               ))}
           </div>
         </div>
